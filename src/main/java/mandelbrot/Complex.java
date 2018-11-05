d0d3b45c21a7dbf3f36a997a5ccf2ebff667668c
Indhi Rousseau <contact@thekinrar.fr>
2018-11-05 14:05:58 +0000

    Fix Complex::divide

--- src/main/java/mandelbrot/Complex.java
+++ src/main/java/mandelbrot/Complex.java
@@ -167,12 +167,13 @@ public class Complex {
      * @return the complex number <code>this / divisor</code>
      */
     Complex divide(Complex divisor) {
-        if (divisor.equals(I)){
-            throw new ArithmeticException("divide by zero");
+        if (divisor.equals(ZERO)){
+            throw new ArithmeticException("Division by zero");
         }
+
         double m = divisor.squaredModulus();
         return new Complex(
-                (this.real + divisor.real + this.imaginary + divisor.imaginary) / m,
+                (this.real * divisor.real + this.imaginary * divisor.imaginary) / m,
                 (this.imaginary * divisor.real - this.real * divisor.imaginary) / m
         );
     }
